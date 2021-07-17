0db3bc617dde083f620b253628fa8999dbfbc013
temptado <jason1287712@gmail.com>
2021-07-17 13:19:30 -0400

    called api

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 
-
-    var requestUrl = 'http://gateway.marvel.com/v1/public/comics?ts=1626533266&apikey=ab9297e9d4bda4ab94cb17eb9e3fe843&hash=119e420fb9eb7795c5ab699ee01f8beb';
-
-function getApi() {
-    
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
-
-  getApi();
-  
-=======
+var PRIV_KEY = "b62c40680e3ea3090a2462bc3021628651c2d45f";
+var PUBLIC_KEY = "ab9297e9d4bda4ab94cb17eb9e3fe843";
+function getMarvelResponse() {
+  // you need a new ts every request                                                                                    
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  // the api deals a lot in ids rather than just the strings you want to use
+  var characterName = 'steve rogers'; // wolverine                                                                             
+  var queryParams = `ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&name=${characterName}`;
+  var url = `http://gateway.marvel.com/v1/public/characters?${queryParams}`;
+  console.log(url);
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var charactersId = data.data.results[0].id;
+      console.log(charactersId);
+      var newParams = `ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&characters=${charactersId}`;
+      characterUrl = `http://gateway.marvel.com/v1/public/comics?${newParams}`;
+      fetch(characterUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then (function (newdata) {
+          console.log(newdata);
+        })
+    });
+}
+getMarvelResponse();
 
 
