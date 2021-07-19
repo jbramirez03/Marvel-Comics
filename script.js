5848f13801141b1eee29c2542786168df16968a1
Jimmy Simpson <simpsonjd12@gmail.com>
2021-07-19 08:10:47 -0400

    added commented function for on key up enter to run the api call

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ var describedComic = document.querySelectorAll(".description-text");
 var PRIV_KEY = "b62c40680e3ea3090a2462bc3021628651c2d45f";
 var PUBLIC_KEY = "ab9297e9d4bda4ab94cb17eb9e3fe843";
 
-
+// need to add function to run when "enter" is pushed 
+// document.querySelector("search_bar").addEventListener("keyup", function(event){
+// if (event.key == "Enter") 
+// to run the same function as below
 
 searchBtn.addEventListener("click", function() {
 
